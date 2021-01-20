b76672f4845a92f9ef050758c87f0e04b78e403b
Daniel Gonzalez <cadagoro@gmail.com>
2021-01-20 01:12:56 -0300

    Animacion de olas en el background de la cabecera

---
+++ main.js
@@ -0,0 +1,15 @@
+// For jQuery
+// var myWave = $('#myID').wavify({
+//     height: 450,
+//     bones: 3,
+//     amplitude: 300,
+//     color: '#333',
+//     speed: .25
+// });
+var myWave = $('#myID2').wavify({
+    height: 650,
+    bones: 3,
+    amplitude: 290,
+    color: '#dc143c',
+    speed: .25
+});
